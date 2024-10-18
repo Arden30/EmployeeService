c25864c2e0104af927e9b73350c39743c8116329
Arsenev Denis <denis.arseniev1995@gmail.com>
2024-10-18 20:49:59 +0300

    feat: testing from main class

--- src/main/java/arden/java/employeeservice/EmployeeServiceApplication.java
+++ src/main/java/arden/java/employeeservice/EmployeeServiceApplication.java
@@ -1,13 +1,23 @@
 package arden.java.employeeservice;
 
+import arden.java.employeeservice.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class EmployeeServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeeServiceApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(EmployeeServiceApplication.class, args);
+        EmployeeService employeeService = applicationContext.getBean(EmployeeService.class);
+        employeeService.findById(1L);
+        employeeService.groupByName();
+        employeeService.findBetween(LocalDate.of(2000, 1, 1), LocalDate.of(2010, 12, 1));
     }
 
 }
